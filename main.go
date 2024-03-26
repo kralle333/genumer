f4e3f417a6c1c1fe883daa61a8880c0ce1b12206
Kristian Pilegaard Jensen <kralle@gmail.com>
2024-03-26 15:13:44 +0100

    changed import

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "genumer/cmd"
+import "github.com/kralle333/genumer/cmd"
 
 //go:generate go run main.go --name "enumStyle" --values "camelCase,pascalCase,upperCase,kebabCase,screamingKebabCase,snakeCase,screamingSnakeCase" --dest "cmd/" --private
 
